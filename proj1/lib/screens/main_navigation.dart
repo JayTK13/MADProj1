e942770e22eaed1250944745dd669caf9d85b609
Jaden Woody <jwoody6@student.gsu.edu>
2026-03-21 19:41:05 -0400

    implement bottom-of-screen navigation buttons with start and history screens

---
+++ proj1/lib/screens/main_navigation.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'start_session_screen.dart';
+import 'session_history_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [StartSessionScreen(), SessionHistoryScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.play_arrow), label: 'Start'),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+        ],
+      ),
+    );
+  }
+}
