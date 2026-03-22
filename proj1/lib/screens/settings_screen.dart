369f8ede88c979ab11bedea8cde12a7c64dd38bf
Jaden Woody <jwoody6@student.gsu.edu>
2026-03-22 12:39:35 -0400

    added settings screen with a dark mode toggle

---
+++ proj1/lib/screens/settings_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool darkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadSettings();
+  }
+
+  Future<void> loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      darkMode = prefs.getBool('darkMode') ?? false;
+    });
+  }
+
+  Future<void> toggleDarkMode(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('darkMode', value);
+
+    setState(() {
+      darkMode = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: SwitchListTile(
+        title: const Text('Dark Mode'),
+        value: darkMode,
+        onChanged: toggleDarkMode,
+      ),
+    );
+  }
+}
