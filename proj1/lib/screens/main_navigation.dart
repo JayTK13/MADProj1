0f28cb7e9779aa4ca86d439655bece4335fce6fc
Jaden Woody <jwoody6@student.gsu.edu>
2026-03-24 10:45:27 -0400

    added appropiate comments to main_navigation.dart

--- proj1/lib/screens/main_navigation.dart
+++ proj1/lib/screens/main_navigation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'start_session_screen.dart';
 import 'session_history_screen.dart';
 
+// Main navigation widget that manages the bottom navigation bar and screen switching
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
 
@@ -10,8 +11,10 @@ class MainNavigation extends StatefulWidget {
 }
 
 class _MainNavigationState extends State<MainNavigation> {
+  // State variable to track the currently selected index of the bottom navigation bar
   int _selectedIndex = 0;
 
+  // List of screens corresponding to each tab in the bottom navigation bar
   List<Widget> get _screens => [StartSessionScreen(), SessionHistoryScreen()];
   void _onItemTapped(int index) {
     setState(() {
@@ -19,10 +22,13 @@ class _MainNavigationState extends State<MainNavigation> {
     });
   }
 
+  // Build method to display the current screen and the bottom navigation bar
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Display the currently selected screen based on the index
       body: _screens[_selectedIndex],
+      // Bottom navigation bar with two items: Start and History
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
