8f348797bb6c648d131f632c29a1eee38b18951f
Jaden Woody <jwoody6@student.gsu.edu>
2026-03-24 10:08:51 -0400

    added appropiate comments to seetings_screen.dart

--- proj1/lib/screens/settings_screen.dart
+++ proj1/lib/screens/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../main.dart';
 
+// Settings screen to toggle dark mode
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
@@ -10,12 +11,13 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  // State variable to track dark mode setting
   bool darkMode = false;
 
   @override
   void initState() {
     super.initState();
-    loadSettings();
+    loadSettings(); // Load settings when the screen initializes
   }
 
   Future<void> loadSettings() async {
@@ -25,6 +27,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
     });
   }
 
+// Function to toggle dark mode and save the setting
   Future<void> toggleDarkMode(bool value) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('darkMode', value);
@@ -35,7 +38,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
     MyApp.of(context)?.toggleTheme(value);
   }
-
+// Build method to display the settings UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
